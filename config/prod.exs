b588b327afbcfc31fd5fc6f51ed25923388722ff
Miguel Barcos <miguel@galar.com>
2017-09-09 18:02:42 +0200

    fix typo in config

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :news, News.Endpoint,
 # Configure your database
 config :news, News.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL")
+  url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
