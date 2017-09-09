ac5bcba7911544befbc6f9746ab08f56d4c8e564
Miguel Barcos <miguel@galar.com>
2017-09-09 18:05:47 +0200

    fix typo in user socket

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule News.UserSocket do
   # channel "room:*", News.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
     timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
